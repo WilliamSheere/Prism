ecb37c235d54ad7012c5324c5daf47b16e58c88f
William Sheere IV <Sheerebilly@yahoo.com>
2025-02-19 20:00:02 -0500

    landing,login,signup.dashboard, dependencies

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,40 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./pages/Error/Error.tsx";
+import { Landing } from "./pages/Landing/Landing.tsx";
+import Login from "./pages/Login/Login.tsx";
+import SignUp from "./pages/SignUp/SignUp.tsx";
+import Dashboard from "./pages/Dashboard/Dashboard.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+		errorElement: <ErrorPage />,
+		children: [
+			{
+				index: true,
+				element: <Landing />,
+			},
+			{
+				path: "/login",
+				element: <Login />,
+			},
+			{
+				path: "/signup",
+				element: <SignUp />,
+			},
+			{
+				path: "/dashboard",
+				element: <Dashboard />,
+			},
+		],
+	},
+]);
+
+const rootElement = document.getElementById("root");
+if (rootElement) {
+	ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
+}
