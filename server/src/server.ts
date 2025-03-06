2bc952617613ea5ce421c429a0a3c1a51beba589
William Sheere IV <Sheerebilly@yahoo.com>
2025-03-05 19:58:30 -0500

    server fixed anded pathway{

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,6 +6,10 @@ import { authenticateToken } from "./utils/auth.js";
 
 import { typeDefs, resolvers } from './schemas/index.js';
 import db from './config/connection.js';
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -30,10 +34,10 @@ const startApolloServer = async () => {
 
   // if we're in production, serve client/dist as static assets
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join(__dirname, '../../client/dist')));
 
     app.get('*', (_req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+      res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
     });
   }
   
