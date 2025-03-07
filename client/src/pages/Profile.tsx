eacb7c971692c213c2c6700b46b5de4e10ad9ab8
William Sheere IV <Sheerebilly@yahoo.com>
2025-03-07 18:34:34 -0500

    fixed unix to correct date format in profile

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -2,6 +2,18 @@ import { useContext, useEffect, Fragment } from "react";
 import { AuthContext } from "../contexts/AuthContext";
 import "../styles/Profile.css";
 
+const formatUnixToDate = (unixTimestamp: number) => {
+	const timestamp =
+		unixTimestamp > 9999999999 ? unixTimestamp / 1000 : unixTimestamp;
+
+	return new Date(timestamp * 1000).toLocaleDateString("en-US", {
+		year: "numeric",
+		month: "long",
+		day: "numeric",
+		hour: "2-digit",
+		minute: "2-digit",
+	});
+};
 const Profile = () => {
 	const { posts } = useContext(AuthContext);
 
@@ -16,7 +28,7 @@ const Profile = () => {
 					<Fragment key={post._id}>
 						<div className="postContainer">
 							<div>{post.username}</div>
-                            <div>{post.createdAt}</div>
+							<div>{formatUnixToDate(post.createdAt)}</div>
 							<div>{post.postText}</div>
 						</div>
 					</Fragment>
